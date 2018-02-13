9020d3055540af882da5daa7ea7beaed0b52a7b2
Voraton Lertrattanapaisal <voraton.l@ku.th>
2018-02-14 03:56:22 +0700

    Sky Generator

---
+++ Assets/Rune Tapping/Scripts/Background/Behaviours/CloudBehaviour.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudBehaviour : MonoBehaviour {
+
+    public Sprite[] cloudSprites;
+    public bool isDirectionRight;
+
+    float speed;
+
+    const float MAXIMUM_BOUND_X = 5f;
+    const float MINIMUM_SPEED = 1f;
+    const float MAXIMUM_SPEED = 5f;
+
+    // Use this for initialization
+    void Start()
+    {
+        ResetCloud();
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (isOutBound())
+        {
+            ResetCloud();
+        }else
+        {
+            Move();
+        }
+	}
+
+    bool isOutBound()
+    {
+        if (isDirectionRight)
+        {
+            return transform.position.x >= MAXIMUM_BOUND_X;
+        }else
+        {
+            return transform.position.x <= -1*MAXIMUM_BOUND_X;
+        }
+    }
+
+    void ResetCloud()
+    {
+        int cloudSpriteIndex = Random.Range(0, cloudSprites.Length);
+        GetComponent<SpriteRenderer>().sprite = cloudSprites[cloudSpriteIndex];
+        speed = Random.Range(MINIMUM_SPEED, MAXIMUM_SPEED);
+        float originPositionX = isDirectionRight ? -1 * MAXIMUM_BOUND_X : MAXIMUM_BOUND_X;
+        transform.position = new Vector2(originPositionX, transform.position.y);
+    }
+
+    void Move()
+    {
+        Vector2 direction = isDirectionRight ? Vector2.right : Vector2.left;
+        transform.Translate(direction * speed * Time.deltaTime);
+    }
+}
---
+++ Assets/Rune Tapping/Scripts/Background/Generators/SkyGenerator.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkyGenerator : MonoBehaviour {
+
+    public GameObject cloud;
+
+    const float MINIMUM_CLOUD_HIGH = -3f;
+    const float CLOUD_LAYER_DEPTH = 1f;
+    const float DISTANCE_BETWEEN_LAYER = 3f;
+    const float MINIMUM_CLOUD_BOUND_X = -5f;
+    const int NUM_CLOUD_LAYER = 3;
+
+	// Use this for initialization
+	void Start () {
+        GenerateSky();
+	}
+	
+	void GenerateSky()
+    {
+        for (int i = 0; i < NUM_CLOUD_LAYER; i++)
+        {
+            float minY = MINIMUM_CLOUD_HIGH + i * DISTANCE_BETWEEN_LAYER;
+            float maxY = minY + CLOUD_LAYER_DEPTH;
+            float y = Random.Range(minY, maxY);
+            bool isDirectionRight = Random.Range(0, 2) == 0;
+            float x = isDirectionRight ? MINIMUM_CLOUD_BOUND_X : -1 * MINIMUM_CLOUD_BOUND_X;
+            GameObject cloudObject = Instantiate(cloud, new Vector2(x, y), Quaternion.identity, transform);
+            cloudObject.GetComponent<CloudBehaviour>().isDirectionRight = isDirectionRight;
+        }
+    }
+}
