4e6014885710db1586d136ecb343428f465122e3
Voraton Lertrattanapaisal <voraton.l@ku.th>
2018-02-14 10:38:39 +0700

    Animation Finish Notifier

---
+++ Assets/Rune Tapping/Scripts/Utils/AnimationFinishNotifier.cs
@@ -0,0 +1,22 @@
+﻿
+using UnityEngine;
+
+public class AnimationFinishNotifier : MonoBehaviour {
+
+    public bool isFinishAnimation;
+
+	// Update is called once per frame
+	void Update () {
+	    if (isFinishAnimation)
+        {
+            FinishAnimation();
+        }	
+	}
+
+    void FinishAnimation()
+    {
+        transform.parent.SendMessage("OnFinishAnimation");
+        gameObject.SetActive(false);
+    }
+
+}
