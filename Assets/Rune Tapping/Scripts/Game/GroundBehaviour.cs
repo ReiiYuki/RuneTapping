e4df73ce2fc2c7d85fb732151789edcf5265b837
Voraton Lertrattanapaisal <voraton.l@ku.th>
2018-02-14 11:09:43 +0700

    Ground Behavior Notify if object hit the ground

--- Assets/Rune Tapping/Scripts/Game/GroundBehaviour.cs
+++ Assets/Rune Tapping/Scripts/Game/GroundBehaviour.cs
@@ -1,21 +1,10 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class GroundBehaviour : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
-
-    private void OnCollisionEnter2D(Collision2D collision)
+    void OnCollisionEnter2D(Collision2D collision)
     {
-        Debug.Log(collision.gameObject);
+        collision.gameObject.SendMessage("OnHitFloor");
     }
+
 }
