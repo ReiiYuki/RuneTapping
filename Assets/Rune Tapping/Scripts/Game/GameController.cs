23a778e35ff1699bf1db1ff52ddd861014190426
Voraton Lertrattanapaisal <voraton.l@ku.th>
2018-02-19 02:13:03 +0700

    Init Some time variable

---
+++ Assets/Rune Tapping/Scripts/Game/GameController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+    float time = MAX_TIME;
+    int score = 0;
+    int combo = 0;
+
+    public const float MAX_TIME = 40;
+    public const float MIN_TIME = 40;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void UpdateTime()
+    {
+        
+    }
+
+}
