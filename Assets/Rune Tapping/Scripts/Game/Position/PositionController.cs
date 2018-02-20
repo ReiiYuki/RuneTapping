501ef0bc2f3e02f9a92d57b6e98b31d7ed0cc5c9
Voraton Lertrattanapaisal <voraton.l@ku.th>
2018-02-20 10:23:48 +0700

    TimeEffect

--- Assets/Rune Tapping/Scripts/Game/Position/PositionController.cs
+++ Assets/Rune Tapping/Scripts/Game/Position/PositionController.cs
@@ -2,10 +2,18 @@
 using UnityEngine;
 
 public class PositionController : MonoBehaviour {
-	
+
+    public GameObject gameController;
+
 	// Update is called once per frame
 	void Update () {
-        transform.Translate(Vector3.up * Time.deltaTime);
+        UpdatePosition();
 	}
+
+    void UpdatePosition()
+    {
+        GameController controller = gameController.GetComponent<GameController>();
+        transform.Translate(Vector3.up * Time.deltaTime * controller.timeEffect);
+    }
 
 }
